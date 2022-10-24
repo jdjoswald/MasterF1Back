e917d91caa8b42b8bb58f3bec0ea8ce53a0d1be3
repconani <repconani@conani.gob.do>
2022-10-24 19:17:26 +0200

    first commit

---
+++ src/main/java/com/ProyectoF1/proyectof1/Proyectof1Application.java
@@ -0,0 +1,13 @@
+package com.ProyectoF1.proyectof1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectof1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectof1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoF1/proyectof1/Proyectof1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoF1.proyectof1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectof1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
